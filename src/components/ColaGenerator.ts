211686f1f2887b7dc4a8d360bc75ef70132ef44e
jihyunglee <jhlee7125@gmail.com>
2023-03-26 20:56:07 +0900

    style: change the directory location since index.html put in root directory

--- src/components/ColaGenerator.ts
+++ src/components/ColaGenerator.ts
@@ -28,7 +28,7 @@ export default class ColaGenerator {
         const item = document.createElement('li');
         const itemTemplate = `
         <button type="button" class="btn-item" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
-          <img src="./assets/img/${el.img}" alt="" class="img-item" />
+          <img src="./src/assets/img/${el.img}" alt="" class="img-item" />
           <strong class="tit-item">${el.name}</strong>
           <span class="txt-price">${el.cost}원</span>
         </button>
