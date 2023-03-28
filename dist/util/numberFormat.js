1be19fdde3a7f6269e8d12fa8237fcf1fe1bee03
jihyunglee <jhlee7125@gmail.com>
2023-03-29 05:36:17 +0900

    build: numberFormat.ts to numberFormat.js

--- dist/util/numberFormat.js
+++ dist/util/numberFormat.js
@@ -1,4 +1,7 @@
 export default function numberFormat(num) {
+    if (num < 1000) {
+        return num;
+    }
     return new Intl.NumberFormat().format(num);
 }
 //# sourceMappingURL=numberFormat.js.map
