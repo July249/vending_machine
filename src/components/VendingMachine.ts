fdd8978c2403e23c0a1f2221b3a60c3b03793246
jihyunglee <jhlee7125@gmail.com>
2023-03-28 21:02:36 +0900

    fix: edit code after code review

--- src/components/VendingMachine.ts
+++ src/components/VendingMachine.ts
@@ -1,31 +1,47 @@
+import numberFormat from '../util/numberFormat';
+
 export default class VendingMachine {
-  private balance: HTMLSpanElement;
-  private itemList: HTMLUListElement;
-  private inputCostEl: HTMLInputElement;
-  private btnPut: HTMLButtonElement;
-  private btnReturn: HTMLButtonElement;
-  private btnGet: HTMLButtonElement;
-  private stagedList: HTMLUListElement;
-  private myMoney: HTMLSpanElement;
-  private gotList: HTMLUListElement;
-  private txtTotal: HTMLElement;
+  #balance: HTMLSpanElement;
+  #itemList: HTMLUListElement;
+  #inputCostEl: HTMLInputElement;
+  #btnPut: HTMLButtonElement;
+  #btnReturn: HTMLButtonElement;
+  #btnGet: HTMLButtonElement;
+  #stagedList: HTMLUListElement;
+  #myMoney: HTMLSpanElement;
+  #gotList: HTMLUListElement;
+  #txtTotal: HTMLElement;
 
   constructor() {
     const vendingMachine = document.querySelector(
       '.vending-machine'
-    )! as HTMLElement;
-    this.balance = vendingMachine.querySelector('.txt-balance')!;
-    this.itemList = vendingMachine.querySelector('.list-item')!;
-    this.inputCostEl = vendingMachine.querySelector('.inp-put')!;
-    this.btnPut = vendingMachine.querySelector('.btn-put')!;
-    this.btnReturn = vendingMachine.querySelector('.btn-return')!;
-    this.btnGet = vendingMachine.querySelector('.btn-get')!;
-    this.stagedList = vendingMachine.querySelector('.list-item-staged')!;
+    ) as HTMLElement;
+    this.#balance = vendingMachine.querySelector(
+      '.txt-balance'
+    ) as HTMLSpanElement;
+    this.#itemList = document.querySelector('.list-item') as HTMLUListElement;
+    this.#inputCostEl = vendingMachine.querySelector(
+      '.inp-put'
+    ) as HTMLInputElement;
+    this.#btnPut = vendingMachine.querySelector(
+      '.btn-put'
+    ) as HTMLButtonElement;
+    this.#btnReturn = vendingMachine.querySelector(
+      '.btn-return'
+    ) as HTMLButtonElement;
+    this.#btnGet = vendingMachine.querySelector(
+      '.btn-get'
+    ) as HTMLButtonElement;
+    this.#stagedList = vendingMachine.querySelector(
+      '.list-item-staged'
+    ) as HTMLUListElement;
 
-    const myinfo = document.querySelector('.my-info')! as HTMLElement;
-    this.myMoney = myinfo.querySelector('.txt-mymoney')!;
-    this.gotList = myinfo.querySelector('.list-item-staged')!;
-    this.txtTotal = myinfo.querySelector('.txt-total')!;
+    const myinfo = document.querySelector('.my-info') as HTMLElement;
+    this.#myMoney = myinfo.querySelector('.txt-mymoney') as HTMLSpanElement;
+    this.#gotList = myinfo.querySelector(
+      '.list-item-staged'
+    ) as HTMLUListElement;
+    this.#txtTotal = myinfo.querySelector('.txt-total') as HTMLElement;
   }
 
   public setup(): void {
@@ -34,6 +50,7 @@ export default class VendingMachine {
 
   private stagedItemGenerator(target: HTMLElement): void {
     const stagedItem = document.createElement('li');
+
     stagedItem.dataset.item = target.dataset.item!;
     stagedItem.dataset.price = target.dataset.price!;
     stagedItem.innerHTML = `
@@ -47,134 +64,155 @@ export default class VendingMachine {
 
     const unstagedBtn = stagedItem.querySelector(
       '.btn-unstaged'
-    )! as HTMLDivElement;
+    ) as HTMLDivElement;
     unstagedBtn.id = `${target.dataset.item}`;
 
-    const imgItem = stagedItem.querySelector('.img-item')! as HTMLImageElement;
+    const imgItem = stagedItem.querySelector('.img-item') as HTMLImageElement;
     imgItem.src = `./src/assets/img/${target.dataset.img}`;
 
-    const titleItem = stagedItem.querySelector('.txt-item')! as HTMLElement;
-    titleItem.textContent = `${target.dataset.item}`;
+    const titleItem = stagedItem.querySelector('.txt-item') as HTMLElement;
+    titleItem.textContent = target.dataset.item || '';
 
     const quantityItem = stagedItem.querySelector(
       '.num-counter'
-    )! as HTMLSpanElement;
+    ) as HTMLSpanElement;
     quantityItem.textContent = '1';
 
-    this.stagedList.appendChild(stagedItem);
+    this.#stagedList.appendChild(stagedItem);
   }
 
   private bindEvents(): void {
-    this.stagedList.addEventListener('click', (e: MouseEvent) => {
-      const targetEl = e.target! as HTMLElement;
+    this.#stagedList.addEventListener('click', (e: MouseEvent) => {
+      const targetEl = e.target as HTMLElement;
       if (targetEl.classList.contains('fa-circle-minus')) {
         const docFrag = document.createDocumentFragment();
-        const unstagedBtn = targetEl.parentElement;
-        const stagedItemList = this.stagedList.querySelectorAll(
-          'li'
-        )! as NodeListOf<HTMLLIElement>;
+        const unstagedBtn = targetEl.parentElement as HTMLDivElement;
+        const stagedItemList = this.#stagedList.querySelectorAll('li');
 
         const updatedStagedItemList: HTMLLIElement[] =
           Array.prototype.filter.call(stagedItemList, (item: HTMLLIElement) => {
             if (item.dataset?.item === unstagedBtn?.id) {
               const quantityItem = item.querySelector(
                 '.num-counter'
-              )! as HTMLSpanElement;
+              ) as HTMLSpanElement;
 
-              const quantity = parseInt(quantityItem.textContent!);
+              if (typeof quantityItem.textContent !== 'string') {
+                return;
+              }
+              const quantity = parseInt(quantityItem.textContent);
 
-              let currentBalance: number = parseInt(
-                this.balance.textContent?.replaceAll(',', '')!
+              let currentBalance = parseInt(
+                this.#balance.textContent?.replaceAll(',', '')!
               );
 
               currentBalance += parseInt(item.dataset?.price!) * quantity;
 
-              this.balance.textContent = new Intl.NumberFormat().format(
-                currentBalance
-              );
+              this.#balance.textContent = numberFormat(currentBalance);
             }
             return item.dataset?.item !== unstagedBtn?.id!;
           });
 
-        for (const list of updatedStagedItemList) {
-          docFrag.appendChild(list);
-        }
+        updatedStagedItemList.forEach((list: HTMLLIElement) =>
+          docFrag.appendChild(list)
+        );
 
-        this.stagedList.innerHTML = '';
-        this.stagedList.append(docFrag);
+        this.#stagedList.innerHTML = '';
+        this.#stagedList.append(docFrag);
       }
     });
 
-    this.btnPut.addEventListener('click', () => {
-      const inputCost = parseInt(this.inputCostEl.value);
+    this.#btnPut.addEventListener('click', () => {
+      if (typeof this.#inputCostEl.value !== 'string') {
+        return;
+      }
+      if (typeof this.#balance.textContent !== 'string') {
+        return;
+      }
+      if (typeof this.#myMoney.textContent !== 'string') {
+        return;
+      }
+
+      const inputCost = parseInt(this.#inputCostEl.value);
       const myMoneyVal = parseInt(
-        this.myMoney.textContent!.replaceAll(',', '')
+        this.#myMoney.textContent.replaceAll(',', '')
       );
       const balanceVal = parseInt(
-        this.balance.textContent!.replaceAll(',', '')
+        this.#balance.textContent.replaceAll(',', '')
       );
 
-      if (inputCost) {
-        if (inputCost <= myMoneyVal) {
-          this.myMoney.textContent =
-            new Intl.NumberFormat().format(myMoneyVal - inputCost) + ' 원';
-          this.balance.textContent = new Intl.NumberFormat().format(
-            (balanceVal ? balanceVal : 0) + inputCost
-          );
-        } else {
-          alert('소지금이 부족합니다.');
-        }
-        this.inputCostEl.value = null!;
+      if (!inputCost) {
+        return;
+      }
+
+      if (inputCost <= myMoneyVal) {
+        const changedValue = myMoneyVal - inputCost;
+        this.#myMoney.textContent = numberFormat(changedValue) + ' 원';
+        this.#balance.textContent = numberFormat(
+          (balanceVal ? balanceVal : 0) + inputCost
+        );
+      } else {
+        alert('소지금이 부족합니다.');
       }
     });
 
-    this.btnReturn.addEventListener('click', () => {
+    this.#btnReturn.addEventListener('click', () => {
+      if (typeof this.#balance.textContent !== 'string') {
+        return;
+      }
+      if (typeof this.#myMoney.textContent !== 'string') {
+        return;
+      }
+
       const balanceVal = parseInt(
-        this.balance.textContent!.replaceAll(',', '')
+        this.#balance.textContent!.replaceAll(',', '')
       );
       const myMoneyVal = parseInt(
-        this.myMoney.textContent!.replaceAll(',', '')
+        this.#myMoney.textContent!.replaceAll(',', '')
       );
 
       if (balanceVal) {
-        this.myMoney.textContent =
-          new Intl.NumberFormat().format(balanceVal + myMoneyVal) + '원';
-        this.balance.textContent = '0';
+        const returnMoney = balanceVal + myMoneyVal;
+        this.#myMoney.textContent = numberFormat(returnMoney) + '원';
+        this.#balance.textContent = '0';
       } else {
         alert('반환될 거스름돈이 없습니다.');
       }
     });
 
-    const btnsCola = this.itemList.querySelectorAll(
-      'button'
-    )! as NodeListOf<HTMLButtonElement>;
+    const btnsCola = this.#itemList.querySelectorAll('button');
 
     btnsCola.forEach((item: HTMLButtonElement) => {
       item.addEventListener('click', (e: MouseEvent) => {
         const targetEl = e.currentTarget as HTMLButtonElement;
         const balanceVal = parseInt(
-          this.balance.textContent!.replaceAll(',', '')
+          this.#balance.textContent!.replaceAll(',', '')
         );
 
         let isStaged = false;
-        const targetElPrice = parseInt(targetEl.dataset.price!);
-        let targetCount = parseInt(targetEl.dataset.count!);
-        const stagedListItem = this.stagedList.querySelectorAll(
-          'li'
-        )! as NodeListOf<HTMLLIElement>;
+        if (!targetEl.dataset.price) {
+          return;
+        }
+        const targetElPrice = parseInt(targetEl.dataset.price);
+        const stagedListItem = this.#stagedList.querySelectorAll('li');
 
         if (balanceVal >= targetElPrice) {
-          this.balance.textContent = new Intl.NumberFormat().format(
-            balanceVal - targetElPrice
-          );
+          const withdraw = balanceVal - targetElPrice;
+          this.#balance.textContent = numberFormat(withdraw);
 
           for (const item of stagedListItem) {
+            if (!item.dataset.item || !targetEl.dataset.item) {
+              return;
+            }
+
             if (item.dataset.item === targetEl.dataset.item) {
               let quantityItem = item.querySelector(
                 '.num-counter'
               )! as HTMLElement;
+              if (typeof quantityItem.textContent !== 'string') {
+                return;
+              }
               quantityItem.textContent = `${
-                parseInt(item.querySelector('.num-counter')!.textContent!) + 1
+                parseInt(quantityItem.textContent) + 1
               }`;
               isStaged = true;
               break;
@@ -186,12 +224,16 @@ export default class VendingMachine {
           }
 
           if (targetEl.dataset.count) {
+            let targetCount = parseInt(targetEl.dataset.count);
             targetCount -= 1;
             targetEl.dataset.count = `${targetCount}`;
           }
 
           if (parseInt(targetEl.dataset.count!) === 0) {
-            targetEl.parentElement?.classList.add('sold-out');
+            if (!targetEl.parentElement) {
+              return;
+            }
+            targetEl.parentElement.classList.add('sold-out');
 
             const warning = document.createElement('em')! as HTMLElement;
             warning.textContent = '해당 상품은 품절입니다.';
@@ -204,39 +246,67 @@ export default class VendingMachine {
       });
     });
 
-    this.btnGet.addEventListener('click', () => {
+    this.#btnGet.addEventListener('click', () => {
       let isGot: boolean = false;
       let totalPrice: number = 0;
 
-      for (const itemStaged of this.stagedList.querySelectorAll('li')) {
-        for (const itemGot of this.gotList.querySelectorAll('li')) {
-          let itemGotCount = itemGot.querySelector(
-            '.num-counter'
-          )! as HTMLSpanElement;
-          if (itemStaged.dataset.item === itemGot.dataset.item) {
-            itemGotCount.textContent = `${
-              parseInt(itemGotCount.textContent!) +
-              parseInt(itemStaged.querySelector('.num-counter')!.textContent!)
-            }`;
-            isGot = true;
-            break;
+      this.#stagedList
+        .querySelectorAll('li')
+        .forEach((itemStaged: HTMLLIElement) => {
+          if (!itemStaged.dataset.item) {
+            return;
           }
-        }
-        if (!isGot) {
-          this.gotList.appendChild(itemStaged);
-        }
-      }
 
-      this.stagedList.innerHTML = '';
+          this.#gotList
+            .querySelectorAll('li')
+            .forEach((itemGot: HTMLLIElement) => {
+              if (!itemGot.dataset.item) {
+                return;
+              }
 
-      this.gotList.querySelectorAll('li').forEach((itemGot) => {
-        totalPrice +=
-          parseInt(itemGot.dataset.price!) *
-          parseInt(itemGot.querySelector('.num-counter')!.textContent!);
+              if (itemStaged.dataset.item === itemGot.dataset.item) {
+                let itemGotCount = itemGot.querySelector(
+                  '.num-counter'
+                ) as HTMLSpanElement;
+                const stagedQuantity = itemStaged.querySelector(
+                  '.num-counter'
+                ) as HTMLSpanElement;
+
+                if (!itemGotCount.textContent || !stagedQuantity.textContent) {
+                  return;
+                }
+
+                itemGotCount.textContent = `${
+                  parseInt(itemGotCount.textContent) +
+                  parseInt(stagedQuantity.textContent)
+                }`;
+                isGot = true;
+              }
+            });
+
+          if (!isGot) {
+            this.#gotList.appendChild(itemStaged);
+          }
+        });
+
+      this.#stagedList.querySelectorAll('li').forEach((item: HTMLLIElement) => {
+        if (!item.dataset.price) {
+          return;
+        }
+
+        if (!item.querySelector('.num-counter')) {
+          return;
+        }
+
+        const itemPrice = parseInt(item.dataset.price);
+        const itemQuantity = parseInt(
+          item.querySelector('.num-counter')!.textContent!
+        );
+
+        totalPrice += itemPrice * itemQuantity;
       });
-      this.txtTotal.textContent = `총금액 : ${new Intl.NumberFormat().format(
-        totalPrice
-      )}원`;
+
+      this.#balance.textContent = numberFormat(totalPrice);
     });
   }
 }
