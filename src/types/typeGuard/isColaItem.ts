43289321b3a4134970b30132a933353295f93aa5
jihyunglee <jhlee7125@gmail.com>
2023-03-29 00:06:07 +0900

    gst

--- src/types/typeGuard/isColaItem.ts
+++ src/types/typeGuard/isColaItem.ts
@@ -1,5 +1,11 @@
 import type { ColaItem } from '../colaItem';
 
 export const isColaItem = (item: any): item is ColaItem[] => {
-  return item.name !== undefined;
+  if (!Array.isArray(item)) return false;
+  if (item.length === 0) return false;
+  if (item[0].name === undefined) return false;
+  if (item[0].cost === undefined) return false;
+  if (item[0].count === undefined) return false;
+  if (item[0].img === undefined) return false;
+  return true;
 };
