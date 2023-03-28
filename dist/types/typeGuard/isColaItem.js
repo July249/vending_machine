b61f9420fa0ee348c730d4059721c3ba798efc49
jihyunglee <jhlee7125@gmail.com>
2023-03-29 00:09:06 +0900

    build: compile typescript to javascript

--- dist/types/typeGuard/isColaItem.js
+++ dist/types/typeGuard/isColaItem.js
@@ -1,4 +1,17 @@
+// ColaItem 타입을 가진 배열인지 확인하는 타입 가드
 export const isColaItem = (item) => {
-    return item.name !== undefined;
+    if (!Array.isArray(item))
+        return false;
+    if (item.length === 0)
+        return false;
+    if (item[0].name === undefined)
+        return false;
+    if (item[0].cost === undefined)
+        return false;
+    if (item[0].count === undefined)
+        return false;
+    if (item[0].img === undefined)
+        return false;
+    return true;
 };
 //# sourceMappingURL=isColaItem.js.map
