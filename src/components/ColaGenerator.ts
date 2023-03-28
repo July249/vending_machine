adde24d2706a6f17848e4935c132ac123aa638f5
jihyunglee <jhlee7125@gmail.com>
2023-03-28 20:52:49 +0900

    fix: edit code after code review

--- src/components/ColaGenerator.ts
+++ src/components/ColaGenerator.ts
@@ -1,15 +1,11 @@
-type Data = {
-  name: string;
-  cost: number;
-  img: string;
-  count: number;
-};
+import type { ColaItem } from '../types/colaItem';
+import { isColaItem } from '../types/typeGuard/isColaItem';
 
 export default class ColaGenerator {
-  itemList: HTMLElement;
+  #itemList: HTMLUListElement;
 
   constructor() {
-    this.itemList = document.querySelector('.list-item')!;
+    this.#itemList = document.querySelector('.list-item') as HTMLUListElement;
   }
 
   async setup(): Promise<void> {
@@ -18,21 +14,25 @@ export default class ColaGenerator {
     });
   }
 
-  async loadData(callback: (arg: []) => void): Promise<void> {
+  async loadData(callback: (arg: ColaItem[]) => void): Promise<void> {
     const res = await fetch(
       'https://raw.githubusercontent.com/July249/vending_machine/main/public/data/item.json'
     );
     if (res.status === 200) {
-      callback(await res.json());
+      const data = (await res.json()) as ColaItem[];
+      if (isColaItem(data)) {
+        callback(data);
+      }
+      Array.isArray(data);
     } else {
       new Error(`Connect Error: ${res.status}`);
     }
   }
 
-  colaFactory(data: Data[] /* JSON data */): void {
+  colaFactory(data: ColaItem[] /* JSON data */): void {
     const docFrag = document.createDocumentFragment();
 
-    data.forEach((el: Data) => {
+    data.forEach((el: ColaItem) => {
       const item = document.createElement('li');
 
       const itemTemplate = `
@@ -45,23 +45,23 @@ export default class ColaGenerator {
 
       item.innerHTML = itemTemplate;
 
-      const buttonItem = item.querySelector('.btn-item')! as HTMLButtonElement;
+      const buttonItem = item.querySelector('.btn-item') as HTMLButtonElement;
       buttonItem.dataset.item = el.name;
       buttonItem.dataset.count = `${el.count}`;
       buttonItem.dataset.price = `${el.cost}`;
       buttonItem.dataset.img = el.img;
 
-      const imgItem = item.querySelector('.img-item')! as HTMLImageElement;
+      const imgItem = item.querySelector('.img-item') as HTMLImageElement;
       imgItem.src = `./src/assets/img/${el.img}`;
 
-      const titleItem = item.querySelector('.tit-item')! as HTMLElement;
+      const titleItem = item.querySelector('.tit-item') as HTMLElement;
       titleItem.textContent = el.name;
 
-      const productCost = item.querySelector('.txt-price')! as HTMLSpanElement;
+      const productCost = item.querySelector('.txt-price') as HTMLSpanElement;
       productCost.textContent = `${el.cost}원`;
 
       docFrag.appendChild(item);
-      this.itemList.appendChild(docFrag);
+      this.#itemList.appendChild(docFrag);
     });
   }
 }
